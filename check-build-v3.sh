a7677e7db26d55584fe0bb7aa45298eca91aeb61
Bruce Becker <brucellino@gmail.com>
2017-08-11 10:21:48 +0200

    Split versions

---
+++ check-build-v3.sh
@@ -0,0 +1,60 @@
+#!/bin/bash -e
+# Copyright 2016 C.S.I.R. Meraka Institute
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+. /etc/profile.d/modules.sh
+module add ci
+module  add  gcc/${GCC_VERSION}
+cd ${WORKSPACE}/build-${BUILD_NUMBER}
+
+echo $?
+
+make install
+mkdir -p ${REPO_DIR}
+mkdir -p modules
+(
+cat <<MODULE_FILE
+#%Module1.0
+## $NAME modulefile
+##
+proc ModulesHelp { } {
+    puts stderr "       This module does nothing but alert the user"
+    puts stderr "       that the [module-info name] module is not available"
+}
+
+module-whatis   "$NAME $VERSION."
+setenv       HEPMC_VERSION       $VERSION
+setenv       HEPMC_DIR           /data/ci-build/$::env(SITE)/$::env(OS)/$::env(ARCH)/$NAME/$VERSION-gcc-${GCC_VERSION}
+prepend-path PATH                           $::env(HEPMC_DIR)/bin
+prepend-path LD_LIBRARY_PATH   $::env(HEPMC_DIR)/lib
+prepend-path GCC_INCLUDE_DIR   $::env(HEPMC_DIR)/include
+prepend-path CFLAGS            "-I$::env(HEPMC_DIR)/include"
+prepend-path LDFLAGS           "-L$::env(HEPMC_DIR)/lib"
+MODULE_FILE
+) > modules/$VERSION-gcc-${GCC_VERSION}
+
+mkdir -vp ${HEP}/${NAME}
+cp -v modules/$VERSION-gcc-${GCC_VERSION} ${HEP}/${NAME}
+echo "is the module available ?"
+module  avail ${NAME}
+echo "Checking the module"
+module add ${NAME}/${VERSION}-gcc-${GCC_VERSION}
+
+echo "checking examples"
+
+echo  "basic tree"
+basic_tree.exe
+
+echo "wrapper"
+hepevt_wrapper_example.exe
---
+++ deploy-v3.sh
@@ -0,0 +1,44 @@
+#!/bin/bash -e
+# this should be run after check-build finishes.
+. /etc/profile.d/modules.sh
+module add deploy
+module add gcc/${GCC_VERSION}
+module add cmake
+cd ${WORKSPACE}/build-${BUILD_NUMBER}
+
+echo "All tests have passed, will now build into ${SOFT_DIR}"
+rm -rf *
+cmake ../${NAME}${VERSION} \
+-DCMAKE_INSTALL_PREFIX=${SOFT_DIR}-gcc-${GCC_VERSION} \
+-DHEPMC_BUILD_EXAMPLES=ON \
+-DHEPMC_INSTALL_INTERFACES=ON
+make
+make install
+
+echo "Creating the modules file directory ${LIBRARIES}"
+mkdir -p ${LIBRARIES}/${NAME}
+(
+cat <<MODULE_FILE
+#%Module1.0
+## $NAME modulefile
+##
+proc ModulesHelp { } {
+    puts stderr "       This module does nothing but alert the user"
+    puts stderr "       that the [module-info name] module is not available"
+}
+
+module-whatis   "$NAME $VERSION : See https://github.com/SouthAfricaDigitalScience/hepmc-deploy"
+setenv HEPMC_VERSION       $VERSION
+setenv HEPMC_DIR           $::env(CVMFS_DIR)/$::env(SITE)/$::env(OS)/$::env(ARCH)/$NAME/$VERSION-gcc-${GCC_VERSION}
+prepend-path PATH                           $::env(HEPMC_DIR)/bin
+prepend-path LD_LIBRARY_PATH   $::env(HEPMC_DIR)/lib
+prepend-path CFLAGS            "-I$::env(HEPMC_DIR)/include"
+prepend-path LDFLAGS           "-L$::env(HEPMC_DIR)/lib"
+MODULE_FILE
+) > ${LIBRARIES}/${NAME}/${VERSION}-gcc-${GCC_VERSION}
+
+echo "checking modulefile"
+module avail ${NAME}
+echo "adding module"
+
+module add ${NAME}/${VERSION}-gcc-${GCC_VERSION}
